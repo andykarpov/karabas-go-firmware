a14fee748fe727f213d059a1ce280ccc7e9e71cd
Andy Karpov <andykarpov@users.noreply.github.com>
2026-03-10 23:10:17 +0100

    Initial version of the wifi/tcp library wrapper to esp8266 at command api (fork/refactored Sparkfun version)

---
+++ lib/Esp8266AT/ESP8266ATClient.h
@@ -0,0 +1,70 @@
+/******************************************************************************
+Esp8266ATClient.h
+Based on SparkFunESP8266Client.h
+ESP8266 WiFi Shield Library Client Header File
+Jim Lindblom @ SparkFun Electronics
+Original Creation Date: June 20, 2015
+http://github.com/sparkfun/SparkFun_ESP8266_AT_Arduino_Library
+
+Development environment specifics:
+	IDE: Arduino 1.6.5
+	Hardware Platform: Arduino Uno
+	ESP8266 WiFi Shield Version: 1.0
+
+This code is beerware; if you see me (or any other SparkFun employee) at the
+local, and you've found our code helpful, please buy us a round!
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#ifndef _ESP8266ATCLIENT_H_
+#define _ESP8266ATCLIENT_H_
+
+#include <Arduino.h>
+#include <IPAddress.h>
+#include "Client.h"
+#include "ESP8266AT.h"
+
+class ESP8266Client : public Client {
+	
+public:
+	ESP8266Client();
+	ESP8266Client(uint8_t sock);
+	ESP8266Client(ESP8266Class* wifi, uint8_t sock);
+
+	uint8_t status();
+	
+	void begin(ESP8266Class* wifi);
+
+	virtual int connect(IPAddress ip, uint16_t port);
+	virtual int connect(const char *host, uint16_t port);
+	
+	int connect(IPAddress ip, uint16_t port, uint32_t keepAlive);
+	int connect(String host, uint16_t port, uint32_t keepAlive = 0);
+	int connect(const char *host, uint16_t port, uint32_t keepAlive);
+	
+	virtual size_t write(uint8_t);
+	virtual size_t write(const uint8_t *buf, size_t size);
+	virtual int available();
+	virtual int read();
+	virtual int read(uint8_t *buf, size_t size);
+	virtual int peek();
+	virtual void flush();
+	virtual void stop();
+	virtual uint8_t connected();
+	virtual operator bool();
+
+	friend class WiFiServer;
+
+	using Print::write;
+
+private:
+	static uint16_t _srcport;
+	uint16_t  _socket;
+	bool ipMuxEn;
+	ESP8266Class* esp8266;
+
+	uint8_t getFirstSocket();
+};
+
+#endif
